2a474b06fa1ea145767bcb08ff18501e9c1e0d9f
vikash984 <vikashk15072001@gmail.com>
2025-08-01 16:08:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import MarkdownEditor from './MarkdownEditor';
+import MarkdownEditor from './components/MarkdownEditor';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="app"> 
+    <div className="app">
       {/* Do not remove the main container */}
       <MarkdownEditor />
     </div>
