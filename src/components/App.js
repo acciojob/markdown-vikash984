c14b2a7ae9abdaa8713485634447fcf6172c0749
vikash984 <vikashk15072001@gmail.com>
2025-08-01 15:59:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-
 import React from 'react';
 import MarkdownEditor from './MarkdownEditor';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="app">
+    <div className="app"> {/* ✅ Main container as required by Cypress */}
       {/* Do not remove the main container */}
       <MarkdownEditor />
     </div>
