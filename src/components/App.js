802a7b66ab053ccb5bd45c3ae1ede186fffec073
vikash984 <vikashk15072001@gmail.com>
2025-08-03 18:27:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import MarkdownEditor from './components/MarkdownEditor';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import "./styles.css";
 
-const App = () => {
+const MarkdownEditor = () => {
+  const [markdown, setMarkdown] = useState("");
+  const [preview, setPreview] = useState("");
+
+  useEffect(() => {
+    setPreview(markdown);
+  }, [markdown]);
+
   return (
     <div className="app">
-      {/* Do not remove the main container */}
-      <MarkdownEditor />
+      <textarea
+        className="textarea"
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+        placeholder="Hello world"
+      />
+      <div className="preview">
+        <ReactMarkdown>{preview}</ReactMarkdown>
+      </div>
     </div>
   );
 };
 
-export default App;
+export default MarkdownEditor;
